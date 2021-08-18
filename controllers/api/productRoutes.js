f6f687f57933de7f3c0efd7af1827b0812f23250
Truong Duong <truong.duong1908@gmail.com>
2021-08-18 13:13:09 -0500

    add routes js file to the controller

---
+++ controllers/api/productRoutes.js
@@ -0,0 +1,94 @@
+const router = require('express').Router();
+const { Product, Category, Cart, Gender } = require('../../models');
+
+// The `/api/products` endpoint
+
+// get all products
+router.get('/', async (req, res) => {
+  // find all products
+  // be sure to include its associated Category and Cart data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get one product
+router.get('/:id', async (req, res) => {
+  // find a single product by its `id`
+  // to include its associated Category and Cart data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, Cart, ProductCart }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'Cannot find this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create new product
+router.post('/', async (req, res) => {
+  // create a new product
+  try {
+    const productData = await Product.create(req.body);
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// update product
+router.put('/:id', async (req, res) => {
+  // update product data
+  Product.update(
+    {
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+    },
+    {
+      where: {
+        id: req.body.id,
+      },
+    }
+  )
+    .then(() => {
+      // Sends the updated book as a json response
+      res.json(req.body);
+    })
+    .catch((err) => res.json(err));
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'Cannot find this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
