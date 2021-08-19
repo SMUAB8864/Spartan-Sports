1f09402d3ae69464e40256a3138ac4e9d188ebf2
James Rydberg <jrrydberg23@gmail.com>
2021-08-19 18:33:57 -0500

    updated checkout page

---
+++ views/js/cart.js
@@ -0,0 +1,17 @@
+
+var $body = $( "body" ),
+page = $body.attr( "id" );
+
+switch( page ) {
+    case "product-list":
+        // actions for handling products
+        break;
+    case "shopping-cart":
+        // actions for handling the shopping cart
+        break;
+    case "checkout":
+        // actions for handling the checkout's page
+        break;
+    default:
+        break;
+}
