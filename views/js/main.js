cd11638fcefcf27f645d2136f3c8bec01c74a523
Mohammed Samod <mohammedlee27@gmail.com>
2021-08-19 11:43:39 -0500

    updated products page, added js for the cart page

--- views/js/main.js
+++ views/js/main.js
@@ -1,25 +1,101 @@
-// $('.login-button').on('click', () => {
-//     $('.login-form').toggle();
-//     $('.login-button').toggleClass('button-active');
-//   });
-
-// $(document).ready(function() {
-//     $("#hidden").hover(funtion() {
-//         $(this).css("color", "black");
-//     },
-//     function() {
-
-//     }
-// })
-$('.slider-two')
-.not(".slick-initialized")
-.slick({
-    // autoplay:true,
-    // autoplayspeed:3000,
-    // dots:true,
-    prevArrow:".site-slider-two .prev",
-    nextArrow:".site-slider-two  .next"
-    slidesToShow:4,
-    slidesToScroll:1,
-    autoplaySpeed: 3000
-});
+const breakpoint = [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 1009,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 460,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ];
+  
+  /**  First Slider */
+  $(".slider-one")
+    .not(".slick-intialized")
+    .slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      prevArrow: ".site-slider .slider-btn .prev",
+      nextArrow: ".site-slider .slider-btn .next"
+    });
+  
+  /**  Second  Slider */
+  $(".slider-two")
+    .not(".slick-intialized")
+    .slick({
+      prevArrow: ".site-slider-two .prev",
+      nextArrow: ".site-slider-two .next",
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplaySpeed: 3000,
+      responsive: breakpoint
+    });
+  
+  /**  Third  Slider */
+  $(".slider-three")
+    .not(".slick-intialized")
+    .slick({
+      prevArrow: ".site-slider-three .prev",
+      nextArrow: ".site-slider-three .next",
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplaySpeed: 3000,
+      infinite: false,
+      responsive: breakpoint
+    });
+  
+  /**  Four  Slider */
+  $(".slider-four")
+    .not(".slick-intialized")
+    .slick({
+      prevArrow: ".site-slider-four .prev",
+      nextArrow: ".site-slider-four .next",
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplaySpeed: 3000,
+      infinite: false,
+      responsive: breakpoint
+    });
+  
+  /**  Five Slider */
+  $(".slider-five")
+    .not(".slick-intialized")
+    .slick({
+      prevArrow: ".slider-brand .prev",
+      nextArrow: ".slider-brand .next",
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplaySpeed: 3000,
+      infinite: true,
+      responsive: breakpoint
+    });
+  
+  /**  Six Slider */
+  $(".slider-six")
+    .not(".slick-intialized")
+    .slick({
+      prevArrow: "",
+      nextArrow: "",
+      autoplaySpeed: 3000,
+      infinite: true,
+      autoplay: true,
+      dots: true
+    });
+  
